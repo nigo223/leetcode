39b9602b5c0a99018edbc689476e4c4c78feec06
shen <shenyifeng@zju.edu.cn>
2019-06-02 20:56:25 +0800

    做了一道dp题

--- algorithm/src/dynamicPlanning/q279/Solution.java
+++ algorithm/src/dynamicPlanning/q279/Solution.java
@@ -1,5 +1,8 @@
 package dynamicPlanning.q279;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
     给定正整数 n，找到若干个完全平方数（比如 1, 4, 9, 16, ...）使得它们的和等于 n。
     你需要让组成和的完全平方数的个数最少。
@@ -16,9 +19,38 @@ package dynamicPlanning.q279;
     解释: 13 = 4 + 9.
 
 */
+
+// 答案
 class Solution {
     public int numSquares(int n) {
-        return 0;
-
+        List<Integer> squareList = generateSquareList(n);
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int square : squareList) {
+                if (square > i) {
+                    break;
+                }
+                min = Math.min(min, dp[i - square] + 1);
+            }
+            dp[i] = min;
+        }
+        return dp[n];
     }
+
+    // 生成小于等于n的平方数数组
+    private List<Integer> generateSquareList(int n) {
+        List<Integer> squareList = new ArrayList<>();
+        int diff = 3;
+        int square = 1;
+
+        while (square <= n) {
+            squareList.add(square);
+            square += diff;
+            diff += 2;
+        }
+        return squareList;
+    }
+
+
 }
